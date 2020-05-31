3fc4a6a696da2c0f88c28731d60712a3c7194e17
hebahkb <heba_kaleem@yahoo.com>
2020-05-31 23:59:34 +0300

    Updates

---
+++ precache-manifest.fc5820d7b45d9de5ed60a00cb9d3c9cf.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf0accc3421f775ccecb1a2671943a0b",
+    "url": "/findteach/index.html"
+  },
+  {
+    "revision": "7b38478ab4751ffd620e",
+    "url": "/findteach/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "51627c06a9de22ef4259",
+    "url": "/findteach/static/css/main.6fae4df3.chunk.css"
+  },
+  {
+    "revision": "7b38478ab4751ffd620e",
+    "url": "/findteach/static/js/2.dce3fb94.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/findteach/static/js/2.dce3fb94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51627c06a9de22ef4259",
+    "url": "/findteach/static/js/main.4507e221.chunk.js"
+  },
+  {
+    "revision": "5e85b362b6357ff8e9ee",
+    "url": "/findteach/static/js/runtime-main.0fb4ff51.js"
+  },
+  {
+    "revision": "466014628e932761401adf12c20b3678",
+    "url": "/findteach/static/media/Tourmaline-Bold.46601462.woff"
+  },
+  {
+    "revision": "8488e78c0bdc26d253a3551c5d7431be",
+    "url": "/findteach/static/media/g1.8488e78c.png"
+  },
+  {
+    "revision": "5589064c2394481e2b780fa602852a48",
+    "url": "/findteach/static/media/g2.5589064c.png"
+  },
+  {
+    "revision": "3f9342becc83f32b1955486d9cf13145",
+    "url": "/findteach/static/media/g3.3f9342be.png"
+  },
+  {
+    "revision": "d72f8002c7ff8f2264da457e240ba76c",
+    "url": "/findteach/static/media/g4.d72f8002.png"
+  },
+  {
+    "revision": "6418e1378398f3c891e4eab7813bd987",
+    "url": "/findteach/static/media/g5.6418e137.png"
+  },
+  {
+    "revision": "9d71ba794c0b4c4dbcb7592b57d41c2c",
+    "url": "/findteach/static/media/g6.9d71ba79.png"
+  },
+  {
+    "revision": "e77d26bd45dc057a12549d059d56ed08",
+    "url": "/findteach/static/media/g7.e77d26bd.png"
+  },
+  {
+    "revision": "095f739606b335dcc0df28fc4a842f6d",
+    "url": "/findteach/static/media/g8.095f7396.png"
+  },
+  {
+    "revision": "617e3e0ca815b0083e9558b3f3799b6a",
+    "url": "/findteach/static/media/g9.617e3e0c.png"
+  }
+]);
